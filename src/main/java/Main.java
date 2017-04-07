527e6cbac93c8874690b16a3a999132110ab2e6a
aliona95 <alionalisovskaja95@gmail.com>
2017-04-07 13:15:35 +0300

    put, post

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,9 +17,9 @@ public class Main {
         get("/people/:id", (req, res) -> gson.toJson(Controller.getPerson(req, res, data)));
         get("/people/name/:name", (req, res) -> gson.toJson(Controller.getByName(req, res, data)));
         get("/people/gender/:gender", (req, res) -> gson.toJson(Controller.getPeopleByGender(req, res, data)));
-        put("/people", (req, res) -> Controller.addPerson(req, res, data));
+        post("/people", (req, res) -> Controller.addPerson(req, res, data));
         delete("/people/:id",(req, res) -> Controller.deletePerson(req, res, data));
-        post("people/:id", (req, res) -> Controller.updatePerson(req, res, data));
+        put("/people/:id", (req, res) -> Controller.updatePerson(req, res, data));
 
         exception(Exception.class, (e, req, res) -> {
             res.status(HTTP_BAD_REQUEST);

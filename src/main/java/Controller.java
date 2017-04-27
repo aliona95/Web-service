4445108697fdbbedc4b074d91311f021baee5308
eima1995 <eimantasbendorius@gmail.com>
2017-04-28 01:27:37 +0300

    pridetas kitas web-servisas

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -130,32 +130,31 @@ public class Controller {
         try{
             int id = Integer.valueOf(request.params("id"));
             Person person = data.getCompany(id);
-
+            int companyId = person.getCompanyId();
             if(person == null){
                 throw new Exception("Nerasta asmens");
             }
-
-            String company = getCompanyData("http://localhost:80/companies/" + id);
-
+	    String company = getCompanyData("http://172.17.0.3:80/companies/" +companyId);
             return company;
+
         }catch(Exception e){
             response.status(HTTP_NOT_FOUND);
             return new ErrorMessage("Nerasta asmens su id " + request.params("id"));
         }
     }
 
-    private static String getCompanyData(String url) throws IOException {
+    private static String getCompanyData(String urlToRead) throws IOException {
         StringBuilder result = new StringBuilder();
-        URL url1 = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
-        connection.setRequestMethod("GET");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while((line = reader.readLine()) != null){
-            result.append(line);
-        }
-        reader.close();
-        return result.toString();
+	URL url = new URL(urlToRead);
+	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	conn.setRequestMethod("GET");
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		rd.close();
+		return result.toString();
     }
 
     public static Object getPeopleByCompany(Request request, Response response, Data data){
